d76d93efa8c46f8b6f98c62432e7271df05e608e
vassilyeva <s6navass@uni-bonn.de>
2019-06-07 01:22:54 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -9,11 +9,57 @@ import torch.cuda as tc
 import torch
 
 
-
+ 
 
 class Settings:
 	def __init__(self):
 		self.test = False
+		'''		
+		# Dataset
+		self.dataset_text 		= '../datasets/Freebase15k/entityWords.txt'
+		#self.dataset_entities 	= '../datasets/Freebase15k/train.txt'
+		self.dataset_vocab 		= '../datasets/Freebase15k/word2id.txt'
+
+	
+		# Word similarity matrix
+		self.use_idf        = True
+		self.similarity     = 'cos'
+		self.similarity_threshold = 0.8
+
+		self.similarity_matrix_filename = path.join(mkdtemp(), 'similarity_matrix.dat')
+
+		self.similarity_function = wn.path_similarity
+
+		self.h5filename = 'similarity_matrix_word2vec.h5'
+		self.h5omega	= 'omega_matrix_word2vec.h5'
+		
+
+		# entity embeddings settings
+		self.transe_method = 'bern'   # bern or unif
+		self.dataset_transe = '../datasets/entity2vec.' + self.transe_method
+
+		# PYTORCH settings
+		self.device = None
+
+		# TEMPORARY SETTINGS:
+		#self.use_pytorch_entities = True
+		
+
+
+		# NMF learning parameters
+		self.lr = .001
+		self.n_epochs = 250
+		self.floatType = tc.FloatTensor # if torch.cuda.is_available else torch.FloatTensor
+		self.longType = tc.LongTensor
+
+		self.n_batches = 100
+		self.n_negative = 3			# number of negative samples for each positive datasample
+
+		self.opt = 'sgd'
+
+		# MODEL settings
+		self.n_features = 50
+		'''
 
 
 	def set(self, args):
@@ -21,8 +67,8 @@ class Settings:
 		if args.dataset == 'test':
 			self.dataset_text 		= '../datasets/Test/entityWords.txt'
 			self.dataset_vocab 		= '../datasets/Test/word2id.txt'
-			self.dataset_transe 	= '../datasets/Test/entity2vec.' + args.transe_method
-			self.test 			= True
+			self.dataset_transe 	= '../datasets/Test/entity2vec.' + args.transe_method	
+			self.test 			= True	
 			#self.n_batches = 10
 		elif args.dataset == 'freebase':
 			self.dataset_text 	= '../datasets/Freebase15k/entityWords.txt'
@@ -45,8 +91,8 @@ class Settings:
 
 		# set up word embedding model
 		if args.word_model == 'word2vec':
-			self.emb_model = Word2Vec
-		else:
+			self.emb_model = Word2Vec 
+		else: 
 			self.emb_model = FastText
 
 		self.incr = args.incr
@@ -54,50 +100,5 @@ class Settings:
 		self.transe_method = args.transe_method
 		self.n_similar = args.n_similar
 		self.use_idf        = args.use_idf
-
-
-'''
-Hyperparams settings for NMF
-'''
-import torch
-
-class Hyperparameters:
-	def __init__(self):
-		self.lambdas = [.01, .01, .2]  # regularizer params for Se, Sw, and model parameters
-		self.momentum = .4
-
-	def set(self, args):
-		self.n_epochs = args.n_epochs
-		self.lr = args.lr
-		self.n_batches = args.n_batches
-		self.n_features = args.n_features
-		self.n_negatives = args.n_negatives
-		self.valid = args.valid
-
-
-		if args.optim == 'sgd':
-			self.optim = torch.optim.SGD
-			self.optim_settings = {'lr': self.lr, 'momentum': self.momentum}
-
-		elif args.optim == 'adam':
-			self.optim = torch.optim.Adam
-			wd = .001
-			self.optim_settings.update({'weight_decay:', wd})
-		else: 		# adagrad
-			self.optim = torch.optim.Adagrad
-			lr_decay = .001
-			self.optim_settings.update({'lr_decay': lr_decay})
-
-	def print_hp(self):
-		settings = ("Optimizer: {}, l_rate: {}, # epochs: {}," \
-			" # batches: {}".format(self.optim.__name__, \
-				self.lr, self.n_epochs, self.n_batches ))
-		print(settings)
-
-	def print_lambdas(self):
-		lambdas_string = ('Regularization params: Se - {}, ' \
-						  'Sw - {}, P - {}'.format(*self.lambdas))
-		print(lambdas_string)
-
 
 
